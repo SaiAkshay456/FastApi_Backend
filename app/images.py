4317423507ff6c324e677540fad6e4ebc0c2c1d8
SaiAkshay456 <saiakshayorg@gmail.com>
2026-01-08 18:01:40 +0530

    learnt about fastapi uploads and integrated imagekit latest version 4x.+

--- app/images.py
+++ app/images.py
@@ -1,9 +1,7 @@
+import os
 from dotenv import load_dotenv
 from imagekitio import ImageKit
-import os
 load_dotenv()
 imageKitClient = ImageKit(
-    os.environ["IMAGEKIT_PRIVATE_KEY"],
-    os.environ["IMAGEKIT_PUBLIC_KEY"],
-    os.environ["IMAGEKIT_URL"]
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
 )
--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.schema import UserCreate
 from sqlalchemy import select
 from app.images import imageKitClient
-
+import math
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     await create_db_and_tables()
@@ -40,16 +40,41 @@ async def get_all_users(session:AsyncSession=Depends(get_async_session)):
     users=await session.execute(select(User))
     return users.scalars().all()
 
+def format_size(size_bytes):
+    if size_bytes == 0:
+        return "0 B"
+    size_name = ("B", "KB", "MB", "GB", "TB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return f"{s} {size_name[i]}"
 @app.post("/upload/file")
 async def uploadFile(
     file:UploadFile=File(...),#required alsi file:UploadFile | None=File() optional category this
     session:AsyncSession=Depends(get_async_session)):
-    response = imageKitClient.upload(
-        file=file.file,
-        file_name=file.filename
-    )
-    return {
-        "url": response["url"],
-        "thumbnail": response["thumbnailUrl"],
-        "fileId": response["fileId"]
-    }
+    filename = file.filename
+    mimetype = file.content_type
+    size_in_bytes = file.size
+    sizeReadable=format_size(size_in_bytes)
+    try:
+        file_content = await file.read()
+        upload_result = imageKitClient.files.upload(
+            file=file_content,
+            file_name=file.filename,
+            use_unique_file_name=True, 
+            folder="/uploads/"
+        )
+        return {
+            "message": "Upload successful",
+            "url": upload_result.url,
+            "file_id": upload_result.file_id,
+            "thumbnail": upload_result.thumbnail_url,
+            "file_metaData":{
+                "name":filename,
+                "file_type":mimetype,
+                "size":size_in_bytes,
+                "readAbleSize":sizeReadable
+            }
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
